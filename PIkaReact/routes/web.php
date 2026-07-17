34c478c8c45b0ba00a9675dfa1757fbdb9c89ae8
DonatBlakaj1 <donatblakaj923@gmail.com>
2026-07-17 14:46:15 +0200

    Delete PIkaReact directory

--- PIkaReact/routes/web.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\BusinessOnboardingController;
-
-Route::get('/', [BusinessOnboardingController::class, 'index'])->name('onboarding');
