2035a4e000fbafadb61478a794a36b69295dc49a
DonatBlakaj1 <donatblakaj923@gmail.com>
2026-07-17 14:41:46 +0200

    Delete Pikaa-main/React-Pika directory

--- Pikaa-main/React-Pika/StepCountrySelection.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import BusinessOnboarding from './pages/BusinessOnboarding';
-
-export const App: React.FC = () => {
-  return <BusinessOnboarding />;
-};
-
-export default App;
